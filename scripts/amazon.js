1de1cb68d43e7d5f0265797e4d4a8144962ab97d
gherlambang <gustiherlambang99@gmail.com>
2024-12-12 17:26:19 +0800

    adding new items and applying destructuring to the amazon.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) =>{
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -59,7 +59,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const {productId} = button.dataset;
 
     let matchingItem;
 
@@ -69,12 +69,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+
     if (matchingItem){
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else{
         cart.push({
-          productId: productId,
-          quantity: 1
+          productId,
+          quantity
         });
     }
     
@@ -85,5 +88,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    console.log(cart);
   });
 });
