740bd1179e48c98f91c0ab42f7341075a78be648
gherlambang <gustiherlambang99@gmail.com>
2024-12-17 17:09:51 +0800

    Learn promises and convert code to promises

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,13 +3,15 @@ import {
   removeFromCart, 
   calculateCartQuantity,
   updateQuantity,
-  updateDeliveryOption} from '../../data/cart.js';
+  updateDeliveryOption
+} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import { loadProducts } from '../../data/products.js';
 
 
 export function renderOrderSummary(){
